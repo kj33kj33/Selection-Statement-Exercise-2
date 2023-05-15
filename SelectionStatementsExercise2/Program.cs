ab1ba068420a25d50d5ef28745482ad47fb8a2e6
James <jamesmcdaniel33@hotmail.com>
2023-05-15 12:18:40 -0500

    initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace SelectionStatementsExercise2
+﻿using static System.Formats.Asn1.AsnWriter;
+using System.Linq.Expressions;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, what is your favorite school subject?");
+
+            var answer = Console.ReadLine();
+
+            switch (answer)
+            {
+                case "physics":
+                    Console.WriteLine("Physics is the study of movement");
+                    break;
+                case "english":
+                    Console.WriteLine("English is a language");
+                    break;
+                case "chemistry":
+                    Console.WriteLine("Chemistry involves elements");
+                    break;
+                case "gym":
+                    Console.WriteLine("Gym class is fun");
+                    break;
+                case "art":
+                    Console.WriteLine("Art class involves creativity");
+                    break;
+                default:
+                    Console.WriteLine($"{answer} is a lovely class!");
+                    break;
+            }
         }
     }
 }
